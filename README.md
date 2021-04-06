3cb81d4e8a5f3a82ec4235d8cc6ed5c4342b3309
Brendan Sheehan <feemcgill@gmail.com>
2021-04-05 21:26:24 -0700

    separate em

--- README.md
+++ README.md
@@ -4,3 +4,26 @@ Eagle Tracks #2
 
 https://codepen.io/oj8kay/pen/rNWdYyE?editors=0010
 https://github.com/mouzofabricio/kaleidoscope
+
+
+```
+{
+    "NORMAL": 0,
+    "ADD": 1,
+    "MULTIPLY": 2,
+    "SCREEN": 3,
+    "OVERLAY": 4,
+    "DARKEN": 5,
+    "LIGHTEN": 6,
+    "COLOR_DODGE": 7,
+    "COLOR_BURN": 8,
+    "HARD_LIGHT": 9,
+    "SOFT_LIGHT": 10,
+    "DIFFERENCE": 11,
+    "EXCLUSION": 12,
+    "HUE": 13,
+    "SATURATION": 14,
+    "COLOR": 15,
+    "LUMINOSITY": 16
+}
+```
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import * as PIXI from 'pixi.js'
-import { PixiPlugin } from 'gsap/PixiPlugin';
 
 import {Linear, TweenMax} from "gsap/TweenMax"
 
@@ -21,66 +20,73 @@ import dp_2_bg from './../assets/dawn_patrol/dp-2-bg.jpg'
 import dp_3 from './../assets/dawn_patrol/dp-3.jpg'
 import dp_4 from './../assets/dawn_patrol/dp-4.jpg'
 
-import Kaleidoscope from './kscope.js'
+import Kaleidoscope from './vibes/kscope.js'
+import PhotoJam from './vibes/photojam.js'
 const state = {}
 
 
-function start_her_up() {
-  initAudio();
 
-  pixi_app.loader
-  .add('dp_1', dp_1)
-  .add('dp_2', dp_2)
-  .add('dp_2_bg', dp_2_bg)
-  .add('vid', vid)
-  .add('dp_3', dp_3)
-  .add('dp_4', dp_4)
-  .load((loader, resources) => {
+pixi_app.loader
+.add('dp_1', dp_1)
+.add('dp_2', dp_2)
+.add('dp_2_bg', dp_2_bg)
+.add('vid', vid)
+.add('dp_3', dp_3)
+.add('dp_4', dp_4)
+.load((loader, resources) => {
 
+
+
+  function start_her_up() {
+    initAudio();
     pixi_app.stage.interactive = true;
     pixi_app.stage
         .on('mousemove', handleMove)
         .on('touchmove', handleMove)
         .on('click', handleClick);  
+  
+    const pj = new PhotoJam(resources.dp_1.texture, () => {
+      console.log('pj callback');
+    })
+    pj.transitionIn();
 
-    var kaleidoscope = new Kaleidoscope(pixi_app, resources.dp_3.texture);
-    kaleidoscope.draw();
-
+    pixi_app.stage.addChild(pj)
+    
     pixi_app.ticker.add(() => {});
-
+  
     function handleMove(e) {}
-
+  
     function handleClick(e) {}
     
+    playTrack(jam)
+  
+  
+    /** RESIZE **/
+    /** RESIZE **/
+    /** RESIZE **/
+    window.addEventListener("resize",function(e){
+      const size = getWindowSize();
+      const w = size.width;
+      const h = size.height;
+      
+      // Scale renderer
+      pixi_app.renderer.view.style.width = w + "px";    
+      pixi_app.renderer.view.style.height = h + "px";      
+      pixi_app.renderer.resize(w,h); 
+    });
+  
+  }
+  
+  
+  
+  
+  var go_button = document.getElementById('go-button');
+  go_button.addEventListener('click', function (event) {
+    start_her_up();
   });
 
 
-  playTrack(jam)
-
-
-
-  /** RESIZE **/
-  /** RESIZE **/
-  /** RESIZE **/
-  window.addEventListener("resize",function(e){
-    const size = getWindowSize();
-    const w = size.width;
-    const h = size.height;
-    
-    // Scale renderer
-    pixi_app.renderer.view.style.width = w + "px";    
-    pixi_app.renderer.view.style.height = h + "px";      
-    pixi_app.renderer.resize(w,h); 
-  });
-    
-  window.addEventListener("resize",debounce(function(e){
-    // Scale scenes
-  }));  
-}
-
-var go_button = document.getElementById('go-button');
-go_button.addEventListener('click', function (event) {
-	start_her_up();
 });
+
 
 export {state}
