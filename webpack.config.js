8c3d76d0affea94e774821600754c731607ee33d
Brendan Sheehan <brendan@adp.bz>
2021-08-03 13:36:17 -0700

    merge in NJ add new webpack config

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path')
+const webpack = require('webpack')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+const pathsToClean = ['dist']
+
+const cleanOptions = {
+  exclude: ['_redirects'],
+  verbose: true,
+  dry: false,
+}
+
+module.exports = (env, options) => {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.(js)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(gif|png|jpe?g|svg|mp4|mp3)$/i,
+          use: [
+            'file-loader',
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                bypassOnDebug: true, // webpack@1.x
+                disable: true, // webpack@2.x and newer
+              },
+            },
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['*', '.js'],
+    },
+    output: {
+      filename: '[name].[hash].js',
+    },
+    devServer: {
+      publicPath: '/',
+      historyApiFallback: true,
+    },
+    devtool: 'eval-source-map',
+    plugins: [
+      new CleanWebpackPlugin(pathsToClean, cleanOptions),
+      new HtmlWebpackPlugin({
+        title: '',
+        description: '',
+        site_url: '',
+        template: './templates/index.html',
+      }),
+      new CopyWebpackPlugin([{ from: './assets/**/*', to: './' }]),
+    ],
+  }
+}
