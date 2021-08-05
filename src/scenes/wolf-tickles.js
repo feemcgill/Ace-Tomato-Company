44415c2fcc31050c8cccef9eca8035aeb49a12ec
Mike Watt <watt.mike@gmail.com>
2021-08-05 14:13:43 -0500

    Wolf Tickles

--- src/scenes/wolf-tickles.js
+++ src/scenes/wolf-tickles.js
@@ -30,36 +30,36 @@ export default class WolfTickles extends PIXI.Container {
     const duration = appState.currentTrackSource
 
 
-     // VidVibe
-     const vv = new VidVibe(pixi_app.loader.resources.wt_vid.url)
-     vv.transitionIn()
-     this.addChild(vv)
-     vv.blendMode = 0
-     vv.alpha = 1
+
+
+    // VidVibe
+    const vv = new VidVibe(pixi_app.loader.resources.wt_vid.url)
+    vv.transitionIn()
+    this.addChild(vv)
+    vv.blendMode = 0
+    vv.alpha = 0
 
 
 
+
+    // PJ 1
     const pj_container = new PIXI.Container()
     this.addChild(pj_container)
 
-    //  const pj = new PhotoJam(pixi_app.loader.resources.wt_2.texture, {
-    //     blendMode: 3,
-    //     moveData: [11, 12, 13, 14],
-    //     amplify: [1, 1.5],
-    //     moveSpeed: 10,
-    //   })
-
-    const pj = new PhotoJam(pixi_app.loader.resources.wt_1.texture, {
-      blendMode: 3,
-      moveData: [110, 120, 130, 140],
-      amplify: [1.2, 1.7],
+    const pj = new PhotoJam(pixi_app.loader.resources.wt_2.texture, {
+      blendMode: 0,
+      moveData: [11, 12, 13, 14],
+      amplify: [0.5, 0.75],
       moveSpeed: 10,
     })
-
+    pj.state.canRotatePointer = false
     pj.transitionIn()
     pj_container.addChild(pj)
-    pj.alpha = 1
+    pj.alpha = 0
 
+
+    
+    // Kaleidscope
     const ks_container = new PIXI.Container()
     this.addChild(ks_container)
 
@@ -73,41 +73,53 @@ export default class WolfTickles extends PIXI.Container {
     ks_container.alpha = 0
 
 
-   
-
-    
     
     // pj2
     const pj2_container = new PIXI.Container()
     this.addChild(pj2_container)
-    const pj2 = new PhotoJam(pixi_app.loader.resources.wt_2.texture, {
+    const pj2 = new PhotoJam(pixi_app.loader.resources.wt_3.texture, {
       blendMode: 3,
       moveData: [11, 12, 13, 14],
-      amplify: [0.5, 1],
+      amplify: [0.5, 0.75],
       moveSpeed: 10,
     })
     pj2.transitionIn()
     pj2_container.addChild(pj2)
     pj2.alpha = 0
 
+    // pj3
+    const pj3_container = new PIXI.Container()
+    this.addChild(pj3_container)
+    const pj3 = new PhotoJam(pixi_app.loader.resources.wt_4.texture, {
+      blendMode: 3,
+      moveData: [11, 12, 13, 14],
+      amplify: [0.5, 0.75],
+      moveSpeed: 10,
+    })
+    pj3.state.canRotatePointer = false
+    pj3.transitionIn()
+    pj3_container.addChild(pj3)
+    pj3.alpha = 0
+
+
     pixi_app.ticker.add(() => {})
 
     this.timeline = new TimelineLite()
-    this.timeline.to(ks_container, 30, { alpha: 0.3, delay: 10 })
-    this.timeline.to(pj, 30, { alpha: 0 }, '-=25')
-    this.timeline.to(vv, 30, { alpha: 1 }, '-=20')
 
-    this.timeline.to(vv, 10, { alpha: 0 }, '95')
-    this.timeline.to(ks_container, 10, { alpha: 0 }, '95')
-    this.timeline.to(pj2, 10, { alpha: 1 }, '95')
+    this.timeline.to(vv, 10, { alpha: 1 })
+    this.timeline.to(ks_container, 10, { alpha: 0.75 }, '10')
+    
+    this.timeline.to(pj, 10, { alpha: 1 }, '15')
+    this.timeline.to(pj, 10, { alpha: 0 }, '35')
 
-    this.timeline.add(() => {
-      pj.setAmplify([0.5, 3])
-    })
+    this.timeline.to(pj2, 10, { alpha: 1 }, '55')
+    this.timeline.to(pj2, 10, { alpha: 0 }, '75')
 
-    this.timeline.add(() => {
-      pj.setAmplify([0.1, 0.4])
-    })
+    this.timeline.to(pj3, 10, { alpha: 1 }, '105')
+    this.timeline.to(pj3, 10, { alpha: 0 }, '115')
+
+    this.timeline.to(ks_container, 10, { alpha: 0 }, '120')
+
 
     if (appState.debug) {
       this.timeline.timeScale(10)
